0d42c24117bcfc43ba5471bde4753bfee0e3f091
maumau27 <mauriciolana@hotmail.com>
2017-06-03 10:06:35 -0300

    Comentando a classe Casa

--- IsoDetetive/src/jogo/Casa.java
+++ IsoDetetive/src/jogo/Casa.java
@@ -7,17 +7,17 @@ import estruturas.*;
 public class Casa {
 
 	// Apenas para Consulta
-	public Vetor2D_int 	position;
-	public CasaType 	type;
+	public Vetor2D_int 	position;//Posição da Casa
+	public CasaType 	type;//Tipo da Casa
 
-
+	//Construtor de Cada
 	public Casa( int x , int y ) {
 		this.position = new Vetor2D_int( x , y );
 		this.type = CasaType.CORREDOR;
 	}
 	
 
-	
+	//Checa se duas Casa são as mesmas, retorna trua casa sim, false caso não
 	public boolean isSameOf( Casa cell ) {
 		if( this.position.x == cell.position.x && this.position.y == cell.position.y ) {
 			return true;
@@ -26,7 +26,7 @@ public class Casa {
 		}
 	}
 	
-
+	//Retorna a distancia de manhattan entre duas Casa
 	public int getManDistTo( Casa cellTo ) {
 		int dist;
 		dist = Math.abs( this.position.x - cellTo.position.x ) + Math.abs( this.position.y + cellTo.position.y );
